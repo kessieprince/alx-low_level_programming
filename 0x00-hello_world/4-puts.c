702f43ab420314ce24abfdbf050af3c0f7d87faf
kessieprince <kessip416@gmail.com.com>
2023-06-19 10:28:04 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,1 +1,11 @@
+#include <stdio.h>
 
+/**
+ * main - a function that print a string for user using a puts function.
+ * Return: 0 (Success).
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
