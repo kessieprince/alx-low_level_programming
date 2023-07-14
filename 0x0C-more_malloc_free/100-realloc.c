8df9cb169f901c998b5d303aea959471d490f14f
kessieprince <kessip416@gmail.com.com>
2023-07-14 03:15:22 -0700

    100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,44 +1,46 @@
-#include "main.h"
-
+#include "holberton.h"
+#include <stdlib.h>
 
 /**
-* _realloc - reallocates a memory block
-* @ptr: pointer to the memory previously allocated with a call to malloc
-* @old_size: size of ptr
-* @new_size: size of the new memory to be allocated
-*
-* Return: pointer to the address of the new memory block
-*/
-
+ * _realloc - reallocates a memory block using malloc and free.
+ * @ptr:  is a pointer to the memory previously allocated with a
+ * call to malloc: malloc(old_size).
+ * @old_size: is the size, in bytes, of the allocated space for ptr
+ * @new_size: is the new size, in bytes of the new memory block
+ *
+ * Return: pointer to reallocated memory, if conditions met,
+ * else, return based on specific conditions
+ */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	void *temp_block;
+	char *rmemb;
+	char *old_p;
 	unsigned int i;
 
-	if (ptr == NULL)
-	{
-		temp_block = malloc(new_size);
-		return (temp_block);
-	}
-	else if (new_size == old_size)
+	if (new_size == old_size)
 		return (ptr);
 
-	else if (new_size == 0 && ptr != NULL)
+	if (!new_size && ptr)
 	{
 		free(ptr);
 		return (NULL);
 	}
-	else
-	{
-		temp_block = malloc(new_size);
-		if (temp_block != NULL)
-		{
-			for (i = 0; i < min(old_size, new_size); i++)
-				*((char *)temp_block + i) = *((char *) ptr + i);
-			free(ptr);
-			return (temp_block);
-		}
-		else
-			return (NULL);
-	}
+
+	if (!ptr)
+		return (malloc(new_size));
+	rmemb = malloc(new_size);
+	if (rmemb == NULL)
+		return (NULL);
+
+	old_p = ptr;
+
+	if (new_size < old_size)
+		for (i = 0; i < new_size; i++)
+			rmemb[i] = old_p[i];
+
+	if (new_size > old_size)
+		for (i = 0; i < old_size; i++)
+			rmemb[i] = old_p[i];
+	free(ptr);
+	return (rmemb);
 }
